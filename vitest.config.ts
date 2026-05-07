e870f8d2e87d45e8981fb2178f9936114ee0ddcf
andersonstack <andersong.pereiracruz@gmail.com>
2026-05-07 20:15:39 -0300

    chore: configurando vitest #30

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="vitest/config" />
+
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        environment: "jsdom",
+        setupFiles: "./src/setupTests.ts",
+        globals: false,
+        include: ['./src/**/*.spec.tsx']
+    }
+})
