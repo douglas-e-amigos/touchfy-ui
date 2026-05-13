f0ab8c31cb92cf78ff93410ba97393a224d26267
andersonstack <andersong.pereiracruz@gmail.com>
2026-05-13 19:36:12 -0300

    feat: criando SecaoHome #26

---
+++ src/app/shared/components/fy-sectionhome/SecaoHome.tsx
@@ -0,0 +1,21 @@
+type SecaoHomeProps = {
+  titulo: string;
+  children: React.ReactNode;
+};
+
+export default function SecaoHome({ titulo, children }: SecaoHomeProps) {
+  return (
+    <section>
+      <div className="flex items-center justify-between gap-5 ml-5 mr-5 mt-2 mb-3">
+        <h2 className="text-2xl font-bold tracking-tight text-white md:text-3xl">
+          {titulo}
+        </h2>
+        <button className="hidden text-sm font-bold text-[#ec268f] transition hover:text-white md:block">
+          Ver tudo
+        </button>
+      </div>
+
+      {children}
+    </section>
+  );
+}
