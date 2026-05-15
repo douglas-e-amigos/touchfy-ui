ff9f667268baefc28748ac039188b2df3e89e2a1
dianaRodriguess <dianarodrigues044@gmail.com>
2026-05-14 22:54:17 -0300

    feat: add FyButtonIcon componente e dep clsx para estilização #27

---
+++ src/app/shared/components/fy-button/FyButtonIcon.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import FyButton from "./FyButton";
+import { ReactNode } from 'react';
+import clsx from "clsx";
+
+interface FyButtonIconProps {
+    icon: ReactNode;
+    onClick?: () => void;
+    variant?: "default" | "outline" | "ghost";
+    className?: string;
+}
+
+export default function FyButtonIcon({ 
+    onClick, 
+    variant,
+    icon, 
+    className,
+}: FyButtonIconProps) {
+    return (
+        <FyButton
+            onClick={onClick}
+            className={clsx("flex items-center justify-center p-2 rounded-full", className)}
+            type={variant}
+        >
+            {icon}
+        </FyButton>
+    );
+}
