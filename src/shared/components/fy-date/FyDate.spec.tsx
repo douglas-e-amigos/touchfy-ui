4bd7ebab731265a84d1190a2744bd1166dab678c
andersonstack <andersong.pereiracruz@gmail.com>
2026-05-15 20:23:10 -0300

    chore: ajuste no teste de FyDate #44

--- src/shared/components/fy-date/FyDate.spec.tsx
+++ src/shared/components/fy-date/FyDate.spec.tsx
@@ -2,16 +2,12 @@ import { render } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import { screen } from "@testing-library/dom";
 import FyDate from "./FyDate";
+import { getDateFormat } from "../../utils/date";
 
 describe("FyDate", () => {
-    it("verificando se a data está correta correta", () => {
-        const date = new Date();
-        const dateComplete = date.toLocaleDateString("pt-BR", {
-            weekday: "long",
-            day: "numeric",
-            month: "long"
-        })
-        render(<FyDate />);
-        expect(screen.getByText(dateComplete))
+    it("verificando se a data está correta", () => {
+        const data = getDateFormat();
+        render(<FyDate data={data}/>);
+        expect(screen.getByText(data))
     })
 })
