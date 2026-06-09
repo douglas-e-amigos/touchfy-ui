4e449e86422f6e5ccafa94118399508bdb9eca17
andersonstack <andersong.pereiracruz@gmail.com>
2026-06-09 14:04:59 -0300

    refactor: ajustando a MediaCard #52

--- src/shared/components/fy-mediacard/MediaCard.tsx
+++ src/shared/components/fy-mediacard/MediaCard.tsx
@@ -3,10 +3,10 @@
 import { useState } from "react";
 
 interface MediaCardProps {
-  imagemURL: string;
-  rodarMusica: () => void;
-  nomeMusica: string;
-  nomeArtista: string;
+  readonly imagemURL: string;
+  readonly rodarMusica: () => void;
+  readonly nomeMusica: string;
+  readonly nomeArtista: string;
 }
 
 export default function MediaCard({
@@ -18,29 +18,37 @@ export default function MediaCard({
   const [mostrarIcone, setMostrarIcone] = useState(false);
 
   return (
-    <div  
+    <div
       role="group"
       aria-label={`Card de mídia da música ${nomeMusica}`}
       onMouseEnter={() => setMostrarIcone(true)}
       onMouseLeave={() => setMostrarIcone(false)}
-      className="rounded-lg bg-[oklch(37%_0.013_285.805)] cursor-pointer w-[23rem] h-[5rem] grid gap-x-4 p-2 items-center [grid-template-areas:'foto_musica_icone'_'foto_artista_icone'] grid-cols-[auto_1fr_auto]"
+      className="overflow-hidden cursor-pointer w-92 h-20 grid gap-x-4 p-2 items-center [grid-template-areas:'foto_musica_icone'_'foto_artista_icone'] grid-cols-[auto_1fr_auto]"
     >
-      <img 
-        className="w-[4rem] h-[4rem] rounded [grid-area:foto]"
-        src={imagemURL} 
-        alt={`Foto de ${nomeArtista}`} 
+      <img
+        className="w-12 h-12 rounded [grid-area:foto]"
+        src={imagemURL}
+        alt={`Foto de ${nomeArtista}`}
       />
 
-      <p className="text-white font-bold [grid-area:musica] self-end">{nomeMusica}</p>
-      <p className="text-gray-400 [grid-area:artista] self-start">{nomeArtista}</p>
+      <p className="text-white font-bold [grid-area:musica] self-end">
+        {nomeMusica}
+      </p>
+      <p className="text-gray-400 [grid-area:artista] self-start">
+        {nomeArtista}
+      </p>
 
-      <div className="relative [grid-area:icone] w-[2.5rem] h-[2.5rem]">
+      <div className="relative [grid-area:icone] w-10 h-10">
         <button
           onClick={abrirMusica}
           className={`absolute left-0 top-1/2 -translate-y-1/2 ${mostrarIcone ? "block" : "hidden"}`}
           aria-label="Ícone play música"
         >
-          <img src="/icons/play-button.svg" alt="Ícone play música" className="w-8 h-8" />
+          <img
+            src="/icons/play-button.svg"
+            alt="Ícone play música"
+            className="w-8 h-8"
+          />
         </button>
       </div>
     </div>
