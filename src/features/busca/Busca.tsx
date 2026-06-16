209d75abc32265362b1a2aa5f386ad0c2d4ad16c
andersonstack <andersong.pereiracruz@gmail.com>
2026-06-16 13:30:35 -0300

    feat: add página de busca #60

---
+++ src/features/busca/Busca.tsx
@@ -0,0 +1,7 @@
+export default function Busca() {
+  return (
+    <header className="w-screen flex items-start flex-col p-4">
+      <h1 className="text-white text-5xl font-bold">Buscar</h1>
+    </header>
+  );
+}
